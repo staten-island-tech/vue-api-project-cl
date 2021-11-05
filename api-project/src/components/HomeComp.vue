812e292db2f836e12268e6c63bcc00a038f72f81
Carol-Student22 <carol.lian22@gmail.com>
2021-11-05 12:26:45 -0400

    .

---
+++ api-project/src/components/HomeComp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+      <h1> lord - {{ pokemon.name }} </h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'dndPreview',
+    props: ['pokemon'], ///remind Whalen later to play around with the props - change from array to a string or object
+
+    ///computed Property == 
+}
+</script>
+
+<style>
+
+</style>
---
+++ api-project/src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <HomeComp />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HomeComp from '@/components/HomeComp.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HomeComp,
+  },
+  data () {
+    return {
+      dndArray: [], ///array
+    };
+  },
+  created: function () {
+    this.fetchData();
+  },
+  methods: {
+    fetchData: async function () {
+      try {
+        const response = await fetch (
+          ////api link
+          'www.dnd5eapi.co/api/'
+        ); //link to the api 
+        const data = await response.json(); // grabs the data (i'm guessing)
+        this.dndArray = data.results; //push data into array (?)
+        console.log(data);
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  }
+}
+</script>
